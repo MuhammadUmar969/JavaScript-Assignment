d7799ee6b3d18f6cc121e6d6d1362f299653e969
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2023-10-14 20:11:33 +0500

    JavaScript Assignment 1

---
+++ JS Assignment 1/index.js
@@ -0,0 +1,143 @@
+//////////////////////////////////////////////////////////////
+
+// PDF 1
+
+//Question 1
+// alert("Hello world");
+
+//Question 2
+// alert("Error! Please enter a valid password");
+
+//Question 3
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+//Question 4
+// alert("Welcome to JS Land");
+// alert("Happy Coding!");
+
+//Question 5
+// alert("Hello... I can run my JS through my web browser's console");
+
+//////////////////////////////////////////////////////////////
+
+// PDF 2
+
+//Question 1
+// var username;
+
+//Question 2
+//var myName = "Muhammad Umar Aziz Khan";
+
+//Question 3
+// a)
+//var message;
+
+//b)
+//message = "Hello World";
+
+//c)
+//alert(message);
+
+//Question 4
+
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var certificate = "Certified Mobile Application Development";
+
+// alert(name);
+// alert(age);
+// alert(certificate);
+
+//Question 5
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+//Question 6
+// var email = "example@example.com";
+
+// alert("My email address is " + email);
+
+//Question 7
+// var book = "A smarter way to learn JavaScript";
+
+// alert(book);
+
+//Question 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//Question 9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+//////////////////////////////////////////////////////////////
+
+// PDF 3
+
+//Question 1
+// var age = 20;
+// alert("I am " + age + " years old");
+
+//Question 2
+// let visitCount = localStorage.getItem('visitCount');
+
+// if (visitCount === null) {
+//     visitCount = 1;
+// } else {
+//     visitCount = parseInt(visitCount) + 1;
+// }
+
+// localStorage.setItem('visitCount', visitCount);
+// alert('You have visited this site ' + visitCount + ' times.');
+
+//Question 3
+// var birthYear =
+//   "My Birth Year is 2003\tData type of declared varaible is nummbeer  ";
+// document.write(birthYear);
+
+//Question 4
+// var name = "John Doe";
+// var order = "5 T-shirt";
+
+// document.write(name + " ordered " + order + "(s) on XYZ Clothing Store");
+
+//////////////////////////////////////////////////////////////
+
+// PDF 4
+
+//Question 1
+//Declare 3 variables
+// var variable1, variable2, variable3;
+
+//Question 2
+//Declare 5 legal & 5 illegal variable names
+
+// 5 Legal
+
+// var myVariable;
+// var variable;
+// var $variable;
+// var _variable;
+// var variable123;
+
+// 5 Ilgeal
+
+// var 123abc;
+// var my-variable;
+// var my variable;
+// var var;
+// var my.variable;
+
+// document.write("Rules for naming JS variables");
+
+// document.write(
+//   "Variable names can only contain , numbers, $ and _. For example: $my_1stVariables "
+// );
+
+// document.write(
+//   "Variables must begin with a letter, $ or _. For example $name, _name or name"
+// );
+
+// document.write("Variable names are case senstive");
+
+// document.write("Variable names should not be JS  Keywords");
